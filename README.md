45aaaed2fa605383b8e45ef77a713946c62b4722
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2016-09-12 15:43:22 +0200

    LIMITS-1: Create the first version of the application.
    * Update README.
    * Introduce translations from l10n.xwiki.org

--- README.md
+++ README.md
@@ -3,9 +3,38 @@
 Enforce some limits on an XWiki instance: number of users, number of wikis, etc...
 
 * Project Lead: [Guillaume Delhumeau](http://www.xwiki.org/xwiki/bin/view/XWiki/gdelhumeau)
-* [Documentation & Download](http://extensions.xwiki.org/xwiki/bin/view/Extension/Limits+Application/) (todo)
+* [Documentation & Download](http://extensions.xwiki.org/xwiki/bin/view/Extension/Limits+Application/)
 * [Issue Tracker](http://jira.xwiki.org/browse/LIMITS)
 * Communication: [Mailing List](http://dev.xwiki.org/xwiki/bin/view/Community/MailingLists), [IRC](http://dev.xwiki.org/xwiki/bin/view/Community/IRC)
 * Minimal XWiki version supported: 7.4.3
 * License: LGPL 2.1
-* [Translations](#) (todo)
+* [Translations](http://l10n.xwiki.org/xwiki/bin/view/Contrib/LimitsApplication)
+
+## Developers
+
+### How to build
+```
+mvn clean install -Pquality --settings maven-settings.xml
+```
+
+### Commit new translations
+To get the translations done on the [l10n.xwiki.org](http://l10n.xwiki.org/xwiki/bin/view/Contrib/LimitsApplication) website and commit them into the application, you need to execute the `get-translations.sh` command:
+
+```
+## Get the translations
+./get-translations.sh
+
+## Look at the new translations
+git status
+
+## Add changes (example)
+git add src/main/resources/TourCode/TourTranslations.fr.xml
+
+## Commit changes
+git commit
+
+## Push them (or make a pull request)
+git push origin master
+```
+
+It should be done before every release.
---
+++ get-translations.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+USER="$L10N_USER"
+PASS="$L10N_PASSWORD"
+
+PROJECT_TRUNKS=`pwd`
+
+function fix_author() {
+    find ./ -name '*.xml' -exec sed -i -e 's#<creator>XWiki.Admin</creator>#<creator>xwiki:XWiki.Admin</creator>#' -e 's#<author>XWiki.Admin</author>#<author>xwiki:XWiki.Admin</author>#' -e 's#<contentAuthor>XWiki.Admin</contentAuthor>#<contentAuthor>xwiki:XWiki.Admin</contentAuthor>#' {} \; -print
+}
+
+function format_xar() {
+    ## due to https://github.com/mycila/license-maven-plugin/issues/37 we need to perform "mvn xar:format" twice.
+    mvn xar:format --settings ${PROJECT_TRUNKS}/maven-settings.xml
+    mvn xar:format --settings ${PROJECT_TRUNKS}/maven-settings.xml
+}
+
+function download_translations() {
+    wget $1 --user="${USER}" --password="${PASS}" --auth-no-challenge -O ./translations.zip ##&&
+    unzip -o translations.zip &&
+    rm translations.zip || $(git clean -dxf && exit -1)
+    fix_author
+}
+
+function read_user_and_password() {
+    if [[ -z "$USER" || -z "$PASS" ]]; then
+        echo -e "\033[0;32mEnter your l10n.xwiki.org credentials:\033[0m"
+        read -e -p "user> " USER
+        read -e -s -p "pass> " PASS
+        echo ""
+    fi
+
+    if [[ -z "$USER" || -z "$PASS" ]]; then
+      echo -e "\033[1;31mPlease provide both user and password in order to be able to get the translations from l10n.xwiki.org.\033[0m"
+      exit -1
+    fi
+}
+
+##
+## Download the translations and format them.
+## Note: it is the responsibility of the developer to actually commit them.
+##
+read_user_and_password
+cd ${PROJECT_TRUNKS}/application-limits-ui/src/main/resources/  || exit -1
+download_translations 'http://l10n.xwiki.org/xwiki/bin/view/L10NCode/GetTranslationFile?name=Contrib.LimitsApplication&app=Contrib'
+cd ${PROJECT_TRUNKS}/application-limits-ui && format_xar
+cd ${PROJECT_TRUNKS}
