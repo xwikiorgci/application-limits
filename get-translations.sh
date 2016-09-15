ecb92eb19eb6bde891d0ff2bf7faf27a0d81a474
Guillaume Delhumeau <guillaume.delhumeau@xwiki.com>
2016-09-15 14:39:29 +0200

    LIMITS-5: Refactor the application to only have a JAR (no XAR anymore).
    * Convert the translations.

--- get-translations.sh
+++ get-translations.sh
@@ -4,21 +4,10 @@ PASS="$L10N_PASSWORD"
 
 PROJECT_TRUNKS=`pwd`
 
-function fix_author() {
-    find ./ -name '*.xml' -exec sed -i -e 's#<creator>XWiki.Admin</creator>#<creator>xwiki:XWiki.Admin</creator>#' -e 's#<author>XWiki.Admin</author>#<author>xwiki:XWiki.Admin</author>#' -e 's#<contentAuthor>XWiki.Admin</contentAuthor>#<contentAuthor>xwiki:XWiki.Admin</contentAuthor>#' {} \; -print
-}
-
-function format_xar() {
-    ## due to https://github.com/mycila/license-maven-plugin/issues/37 we need to perform "mvn xar:format" twice.
-    mvn xar:format --settings ${PROJECT_TRUNKS}/maven-settings.xml
-    mvn xar:format --settings ${PROJECT_TRUNKS}/maven-settings.xml
-}
-
 function download_translations() {
     wget $1 --user="${USER}" --password="${PASS}" --auth-no-challenge -O ./translations.zip ##&&
     unzip -o translations.zip &&
     rm translations.zip || $(git clean -dxf && exit -1)
-    fix_author
 }
 
 function read_user_and_password() {
@@ -40,7 +29,6 @@ function read_user_and_password() {
 ## Note: it is the responsibility of the developer to actually commit them.
 ##
 read_user_and_password
-cd ${PROJECT_TRUNKS}/application-limits-ui/src/main/resources/  || exit -1
+cd ${PROJECT_TRUNKS}/application-limits-api/src/main/resources/  || exit -1
 download_translations 'http://l10n.xwiki.org/xwiki/bin/view/L10NCode/GetTranslationFile?name=Contrib.LimitsApplication&app=Contrib'
-cd ${PROJECT_TRUNKS}/application-limits-ui && format_xar
 cd ${PROJECT_TRUNKS}
